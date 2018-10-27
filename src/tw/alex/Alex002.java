606ca9ea7b839f2a587ccda0ba1069619fdb4df2
Alex <alexs880121@gmail.com>
2018-10-27 12:18:42 +0800

    v1

---
+++ src/tw/alex/Alex002.java
@@ -0,0 +1,37 @@
+package tw.alex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Alex002")
+public class Alex002 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.setContentType("text/html; charset=UTF-8");
+//		PrintWriter out = response.getWriter();	
+//		
+//		
+//		String account = request.getParameter("account");
+//		String passwd = request.getParameter("password");
+//		System.out.println(account + ":" + passwd);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();	
+		
+		
+		String account = request.getParameter("account");
+		String passwd = request.getParameter("password");
+		System.out.println(account + ":" + passwd);
+	}
+
+}
